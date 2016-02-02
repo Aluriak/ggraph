467b602d29d933796f4c7bfac97b4d3976477731
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-02-02 11:54:11 +0100

    main: show a problem of reimporting

--- ggraph/__main__.py
+++ ggraph/__main__.py
@@ -2,8 +2,10 @@
 # std import
 import sys
 
+print('This is a print statement in the __main__.py file. '
+      'This can\'t be printed more than once.')
 # package import
-from .application import app
+from ggraph.application import app
 
 if __name__ == '__main__':
     # Ok it's hugly but it's run, some day we use a real configuration system
